3b2811d3874208e0b182a344ea4addb7e9778041
Alex Cwiek <a.i.cwiek@gmail.com>
2017-08-16 17:19:54 +0100

    show route works

--- src/js/config/router.js
+++ src/js/config/router.js
@@ -11,6 +11,11 @@ function Router($stateProvider, $urlRouterProvider, $locationProvider) {
     url: '/trips',
     templateUrl: 'js/views/trips/index.html',
     controller: 'TripIndexCtrl as tripsIndex'
+  })
+  .state('tripsShow', {
+    url: '/trips/:id',
+    templateUrl: 'js/views/trips/show.html',
+    controller: 'TripShowCtrl as tripsShow'
   });
   $urlRouterProvider.otherwise('/');
 }
--- src/js/controllers/trips.js
+++ src/js/controllers/trips.js
@@ -1,10 +1,26 @@
 angular
   .module('tripApp')
-  .controller('TripIndexCtrl', TripIndexCtrl);
+  .controller('TripIndexCtrl', TripIndexCtrl)
+  .controller('TripShowCtrl', TripShowCtrl);
 
 TripIndexCtrl.$inject = ['Trip'];
 function TripIndexCtrl(Trip) {
   const vm = this;
 
   vm.all = Trip.query();
+}
+
+TripShowCtrl.$inject = ['Trip', '$stateParams', '$state'];
+function TripShowCtrl(Trip, $stateParams, $state) {
+  const vm = this;
+
+  vm.trip = Trip.get($stateParams);
+
+  function tripsDelete() {
+    vm.trip
+      .$remove()
+      .then(() => $state.go('tripsIndex'));
+  }
+
+  vm.delete = tripsDelete;
 }
