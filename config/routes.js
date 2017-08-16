fd078da051e87ca8bacc10de8ac6a564b4770ac9
Alex Cwiek <a.i.cwiek@gmail.com>
2017-08-16 15:51:19 +0100

    restful api for countries

--- config/routes.js
+++ config/routes.js
@@ -5,4 +5,11 @@ router.route('/trips')
   .get(tripsController.indexTripRoute)
   .post(tripsController.createTripRoute);
 
+router.route('/trips/:id')
+  .get(tripsController.showTripRoute)
+  .delete(tripsController.deleteTripRoute);
+
+router.route('/trips/:id/edit')
+  .put(tripsController.updateTripRoute);
+
 module.exports = router;
--- controllers/trips.js
+++ controllers/trips.js
@@ -14,7 +14,49 @@ function createTripRoute(req, res, next){
   .then((trip) => res.status(201).json(trip))
   .catch(next);
 }
+
+function showTripRoute(req, res, next){
+  Trip
+  .findById(req.params.id)
+  .exec()
+  .then(trip => {
+    if(!trip) return res.notFound();
+    return res.json(trip);
+  })
+  .catch(next);
+}
+
+function updateTripRoute(req, res, next){
+  Trip
+    .findById(req.params.id)
+    .exec()
+    .then(trip => {
+      if(!trip) return res.notFound();
+
+      trip = Object.assign(trip, req.body);
+      return trip.save();
+    })
+    .then(trip => res.json(trip))
+    .catch(next);
+}
+
+function deleteTripRoute(req, res, next) {
+  Trip
+    .findById(req.params.id)
+    .exec()
+    .then((trip) => {
+      if(!trip) return res.notFound();
+
+      return trip.remove();
+    })
+    .then(() => res.status(204).end())
+    .catch(next);
+}
+
 module.exports = {
   indexTripRoute,
-  createTripRoute
+  createTripRoute,
+  showTripRoute,
+  updateTripRoute,
+  deleteTripRoute
 };
